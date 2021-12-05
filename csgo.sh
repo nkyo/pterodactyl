b90383e965edbad91f90b2d6438d57e7e276c8ea
Nam Kyo <namkyo@outlook.jp>
2021-12-05 10:35:42 +0700

    Update csgo.sh

--- csgo.sh
+++ csgo.sh
@@ -4,7 +4,7 @@
 # Server Files: /mnt/server
 # Image to install with is 'ubuntu:18.04'
 apt -y update
-apt -y --no-install-recommends install curl lib32gcc1 ca-certificates unzip
+apt -y --no-install-recommends install curl lib32gcc1 ca-certificates
 
 ## just in case someone removed the defaults.
 if [ "${STEAM_USER}" == "" ]; then
@@ -38,26 +38,10 @@ mkdir -p /mnt/server/.steam/sdk64
 cp -v linux64/steamclient.so ../.steam/sdk64/steamclient.so
 
 cd /mnt/server/csgo/
-wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6528-linux.tar.gz
-wget https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1145-linux.tar.gz
-wget https://ptah.zizt.ru/files/PTaH-V1.1.3-build20-linux.zip
-wget https://github.com/kgns/weapons/releases/download/v1.7.5/weapons-v1.7.5.zip
-wget https://github.com/nkyo/kniferound/releases/download/latest/kniferound.zip
-tar -xvf sourcemod-1.10.0-git6528-linux.tar.gz
-tar -xvf mmsource-1.11.0-git1145-linux.tar.gz
-rm -f sourcemod-1.10.0-git6528-linux.tar.gz && rm -f mmsource-1.11.0-git1145-linux.tar.gz
-unzip PTaH-V1.1.3-build20-linux.zip
-unzip weapons-v1.7.5.zip
-unzip kniferound.zip
+wget https://github.com/nkyo/pterodactyl/releases/download/latest/nkCSGO.tar
+tar -xvf nkCSGO.tar
+rm -f nkCSGO.tar
 sed -i '/\"FollowCSGOServerGuidelines\"/d' addons/sourcemod/configs/core.cfg
 sed -i '/\}/d' addons/sourcemod/configs/core.cfg
 echo "\"FollowCSGOServerGuidelines\" \"no\"" >> addons/sourcemod/configs/core.cfg
 echo "}" >> addons/sourcemod/configs/core.cfg
-rm -f PTaH-V1.1.3-build20-linux.zip && rm -f weapons-v1.7.5.zip && rm -f kniferound.zip
-wget https://github.com/Franc1sco/Franug-AgentsChooser/archive/master.zip
-unzip master.zip
-mv Franug-AgentsChooser-master/* /mnt/server/csgo/addons/sourcemod/plugins/
-wget https://github.com/quasemago/CSGO_WeaponStickers/releases/download/v1.0.13c/weaponstickers_1.0.13c.zip
-unzip weaponstickers_1.0.13c.zip
-mv weaponstickers_1.0.13c/* ..
-rm -f weaponstickers_1.0.13c.zip
